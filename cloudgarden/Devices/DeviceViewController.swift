311644d97285f0c529352eed401f432c712ca1ff
Mia Bosheva <boshevam@gmail.com>
2024-04-03 16:25:38 +0200

    Project and storyboards setup

---
+++ cloudgarden/Devices/DeviceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceViewController.swift
+//  cloudgarden
+//
+//  Created by Mia Bosheva on 3.4.24.
+//
+
+import Foundation
+import UIKit
+
+
+class DeviceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ cloudgarden/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  cloudgarden
+//
+//  Created by Mia Bosheva on 3.4.24.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
