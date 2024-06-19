287c1f3ef09e516547d13b2e3629000d4e851b95
Mia Bosheva <mia.bosheva@students.finki.ukim.mk>
2024-06-20 00:15:37 +0200

    Image Picker functionality added

--- cloudgarden/Views/Home/Profile/ProfileView.swift
+++ cloudgarden/Views/Home/Profile/ProfileView.swift
@@ -10,6 +10,9 @@ struct ProfileView: View {
     @State private var deviceCount: Int = 0
     @State private var plantCount: Int = 0
     
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerPresented = false
+    
     init(userModel: UserModel, deviceAndPlantModel: DeviceAndPlantModel) {
         self.userModel = userModel
         self.deviceAndPlantModel = deviceAndPlantModel
@@ -69,7 +72,7 @@ struct ProfileView: View {
                             .padding(.bottom, 8)
                             
                             Button {
-                               // TODO: - Backend reset API call
+                                // TODO: - Backend reset API call
                             } label: {
                                 RoundedRectangle(cornerRadius: 27)
                                     .frame(maxWidth: .infinity, maxHeight: 44, alignment: .center)
@@ -108,14 +111,38 @@ struct ProfileView: View {
                 VStack {
                     ZStack {
                         ZStack{
-                            Circle()
-                                .foregroundColor(.customLimeGreen)
-                                .frame(height: 140)
-                            Circle()
-                                .foregroundColor(.customOffWhite)
-                                .frame(height: 134)
-                            Text("👤")
-                                .font(.system(size: 50))
+                            Image(uiImage: selectedImage ?? .defaultPlant)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 140, height: 140)
+                                .clipShape(Circle())
+                                .overlay {
+                                    Circle().stroke(.customOffWhite, lineWidth: 4)
+                                }
+                            VStack {
+                                
+                                Spacer()
+                                
+                                HStack {
+                                    
+                                    Button {
+                                        self.isImagePickerPresented.toggle()
+                                    } label: {
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .frame(width: 80, height: 44, alignment: .center)
+                                            .foregroundColor(.customLimeGreen)
+                                            .overlay{
+                                                Text("Change Photo")
+                                                    .font(.system(size: 14))
+                                                    .foregroundColor(.white)
+                                            }
+                                    }
+                                    
+                                    Spacer()
+                                }
+                            }
+                            .frame(width: 144, height: 144)
+                            
                         }
                     }
                     .padding(.top, 42)
@@ -125,9 +152,50 @@ struct ProfileView: View {
                 
             }
         }
+        .sheet(isPresented: $isImagePickerPresented) {
+            ImagePicker(selectedImage: $selectedImage)
+        }
         .onAppear {
             self.deviceCount = deviceAndPlantModel.devices.count
             self.plantCount = deviceAndPlantModel.plants.count
         }
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) private var presentationMode
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
     }
 }
